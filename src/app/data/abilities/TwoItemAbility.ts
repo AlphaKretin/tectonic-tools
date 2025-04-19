18b8a4e6b0d303058b395181cefe4b512ed9d5a4
AlphaKretin <deewhydeeecks@gmail.com>
2025-04-20 00:55:21 +0930

    Fix item type flags

--- src/app/data/abilities/TwoItemAbility.ts
+++ src/app/data/abilities/TwoItemAbility.ts
@@ -1,6 +1,7 @@
 import { LoadedAbility } from "../loading/abilities";
 import { Ability } from "../types/Ability";
 import { Item } from "../types/Item";
+import { isNull } from "../util";
 
 type validateItemFunction = (items: Item[]) => boolean;
 
@@ -14,20 +15,28 @@ function allFlag(items: Item[], flag: string) {
 }
 
 export const twoItemAbilities: Record<string, validateItemFunction> = {
-    ALLTHATGLITTERS: (items) => allFlag(items, "Gem") && differentItems(items),
+    ALLTHATGLITTERS: (items) => allFlag(items, "TypeGem") && differentItems(items),
     BERRYBUNCH: (items) => allFlag(items, "Berry") && differentItems(items),
     CLUMSYKINESIS: () => true,
     FASHIONABLE: (items) =>
         items.filter((i) => i.flags.includes("Clothing")).length === 1 &&
         items.filter((i) => !i.flags.includes("Clothing")).length === 1 &&
         differentItems(items),
-    HERBALIST: (items) => allFlag(items, "Gem") && differentItems(items),
+    HERBALIST: (items) => allFlag(items, "Herb") && differentItems(items),
 };
 
 export class TwoItemAbility extends Ability {
-    validate: validateItemFunction;
+    private validate: validateItemFunction;
     constructor(ability: LoadedAbility, validate: validateItemFunction) {
         super(ability);
         this.validate = validate;
+    }
+
+    public validateItems(items: Item[]): boolean {
+        // allow anything if an item remains null, since constraints should only apply if both items are (to be) selected
+        if (items.some((i) => isNull(i))) {
+            return true;
+        }
+        return this.validate(items);
     }
 }
